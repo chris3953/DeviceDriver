5613fb8113c4c2309313e6f8beeffdf30ae5a888
student <you@example.com>
2023-08-01 01:29:58 -0700

    getting somewhere

--- Module/encryptor.c
+++ Module/encryptor.c
@@ -10,28 +10,49 @@
 #define CALC_IOC_MAGIC 'c'
 #define CALC_IOC_SUM _IOWR(CALC_IOC_MAGIC, 1, int)
 
+#define CAESAR_KEY 3 // You can change the Caesar key here
+
 static int major_number;
+static char result_str[100];
 static int result = 0; // Define the 'result' variable here
 
-static int calculator_open(struct inode *inode, struct file *file)
+// Function to encrypt/decrypt a single character using the Caesar cipher
+// Function to encrypt/decrypt a single character using the Caesar cipher
+static char caesar_cipher(char ch, int key)
+{
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        ch = 'A' + (ch - 'A' + key + 26) % 26;
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        ch = 'a' + (ch - 'a' + key + 26) % 26;
+    }
+    return ch;
+}
+
+static int encryptor_open(struct inode *inode, struct file *file)
 {
     printk(KERN_INFO "Encryptor device opened\n");
     return 0;
 }
 
-static int calculator_release(struct inode *inode, struct file *file)
+static int encryptor_release(struct inode *inode, struct file *file)
 {
     printk(KERN_INFO "Encryptor device released\n");
     return 0;
 }
 
-static ssize_t calculator_read(struct file *file, char __user *user_buffer, size_t count, loff_t *offset)
+static ssize_t encryptor_read(struct file *file, char __user *user_buffer, size_t count, loff_t *offset)
 {
-    int len;
-    char result_str[16];
+    int len = strlen(result_str);
+    int i;
 
-    snprintf(result_str, sizeof(result_str), "%d\n", result);
-    len = strlen(result_str);
+    // Encrypt the result string before sending it to the user
+    for (i = 0; i < len; ++i)
+    {
+        result_str[i] = caesar_cipher(result_str[i], CAESAR_KEY);
+    }
 
     if (copy_to_user(user_buffer, result_str, len) != 0)
         return -EFAULT;
@@ -39,20 +60,30 @@ static ssize_t calculator_read(struct file *file, char __user *user_buffer, size
     return len;
 }
 
-static ssize_t calculator_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *offset)
+static ssize_t encryptor_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *offset)
 {
-    char input_str[16];
+    char input_str[100];
+    int i;
+
+    if (count >= sizeof(input_str))
+        return -EINVAL;
 
     if (copy_from_user(input_str, user_buffer, count) != 0)
         return -EFAULT;
 
+    // Decrypt the input string before storing it as the result
+    for (i = 0; i < count; ++i)
+    {
+        input_str[i] = caesar_cipher(input_str[i], -CAESAR_KEY);
+    }
+
     input_str[count] = '\0';
-    sscanf(input_str, "%d", &result);
+    snprintf(result_str, sizeof(result_str), "%s", input_str);
 
     return count;
 }
 
-static long calculator_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static long encryptor_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
     int input;
 
@@ -65,29 +96,31 @@ static long calculator_ioctl(struct file *file, unsigned int cmd, unsigned long 
     if (copy_from_user(&input, (int *)arg, sizeof(int)) != 0)
         return -EFAULT;
 
-    switch (cmd) {
-        case CALC_IOC_SUM:
-            result += input;
-            break;
-        default:
-            return -ENOTTY;
+    switch (cmd)
+    {
+    case CALC_IOC_SUM:
+        result += input;
+        break;
+    default:
+        return -ENOTTY;
     }
 
     return 0;
 }
 
-static struct file_operations calculator_fops = {
-    .open = calculator_open,
-    .release = calculator_release,
-    .read = calculator_read,
-    .write = calculator_write,
-    .unlocked_ioctl = calculator_ioctl,
+static struct file_operations encryptor_fops = {
+    .open = encryptor_open,
+    .release = encryptor_release,
+    .read = encryptor_read,
+    .write = encryptor_write,
+    .unlocked_ioctl = encryptor_ioctl,
 };
 
-static int __init calculator_init(void)
+static int __init encryptor_init(void)
 {
-    major_number = register_chrdev(0, DEVICE_NAME, &calculator_fops);
-    if (major_number < 0) {
+    major_number = register_chrdev(0, DEVICE_NAME, &encryptor_fops);
+    if (major_number < 0)
+    {
         printk(KERN_ALERT "Failed to register encryptor device driver\n");
         return major_number;
     }
@@ -96,14 +129,14 @@ static int __init calculator_init(void)
     return 0;
 }
 
-static void __exit calculator_exit(void)
+static void __exit encryptor_exit(void)
 {
     unregister_chrdev(major_number, DEVICE_NAME);
     printk(KERN_INFO "Encryptor device driver unloaded\n");
 }
 
-module_init(calculator_init);
-module_exit(calculator_exit);
+module_init(encryptor_init);
+module_exit(encryptor_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Christian Mcglothen");
--- Test/Mcglothen_Christian_HW6_main.c
+++ Test/Mcglothen_Christian_HW6_main.c
@@ -3,46 +3,96 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <string.h> // Include the string.h header for strlen
 
 #define DEVICE_PATH "/dev/encryptor"
-#define ENCRYPT _IO('k', 1)
-#define DECRYPT _IO('k', 2)
+#define CALC_IOC_MAGIC 'c'
+#define CALC_IOC_SUM _IOWR(CALC_IOC_MAGIC, 1, int)
 
-int main()
-{
-    int fd, result;
-    char message[256];
+// Caesar key used in the driver (should match the value in the driver)
+#define CAESAR_KEY 3
 
+// Function to encrypt/decrypt a single character using the Caesar cipher
+static char caesar_cipher(char ch, int key) {
+    if (ch >= 'A' && ch <= 'Z') {
+        ch = 'A' + (ch - 'A' + key + 26) % 26;
+    } else if (ch >= 'a' && ch <= 'z') {
+        ch = 'a' + (ch - 'a' + key + 26) % 26;
+    }
+    return ch;
+}
+
+int main() {
+    int fd;
+    int ret, data;
+    char buffer[100];
+    char decrypt[100]; 
+    
+
+    // Open the device file
     fd = open(DEVICE_PATH, O_RDWR);
     if (fd < 0) {
-        printf("Failed to open the encryptor device\n");
+        perror("Failed to open the device file");
         return -1;
     }
 
-    // Encrypt mode
-    ioctl(fd, ENCRYPT);
+    // Test write: send data to the device
+    printf("Enter a message to encrypt: ");
+    fgets(buffer, sizeof(buffer), stdin);
 
-    // Writing the message to encrypt
-    printf("Enter the message to encrypt: ");
-    fgets(message, sizeof(message), stdin);
-    write(fd, message, strlen(message));
+    // Remove the trailing newline character, if present
+    int len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
 
-    // Reading the encrypted message
-    read(fd, message, sizeof(message));
-    printf("Encrypted message: %s\n", message);
+    ret = write(fd, buffer, strlen(buffer));
+    if (ret < 0) {
+        perror("Failed to write to the device");
+        close(fd);
+        return -1;
+    }
 
-    // Decrypt mode
-    ioctl(fd, DECRYPT);
+    // Test read: receive the encrypted data from the device
+    ret = read(fd, buffer, sizeof(buffer));
+    if (ret < 0) {
+        perror("Failed to read from the device");
+        close(fd);
+        return -1;
+    }
+    
 
-    // Writing the message to decrypt
-    printf("Enter the message to decrypt: ");
-    fgets(message, sizeof(message), stdin);
-    write(fd, message, strlen(message));
+    // Test ioctl: add a value to the result
+    data = 10;
+    ret = ioctl(fd, CALC_IOC_SUM, &data);
+    if (ret < 0) {
+        perror("Failed to perform ioctl");
+        close(fd);
+        return -1;
+    }
 
-    // Reading the decrypted message
-    read(fd, message, sizeof(message));
-    printf("Decrypted message: %s\n", message);
+    // Test read after ioctl: receive the updated encrypted message from the device
+    ret = read(fd, buffer, sizeof(buffer));
+    if (ret < 0) {
+        perror("Failed to read from the device");
+        close(fd);
+        return -1;
+    }
+    printf("Updated encrypted message: %s\n", buffer);
 
+    printf("Enter a message to decrypt: "); 
+
+    fgets(decrypt, sizeof(decrypt), stdin);
+
+    // Decrypt the received message
+    for (int i = 0; i < ret; ++i) {
+        decrypt[i] = caesar_cipher(decrypt[i], -CAESAR_KEY);
+    }
+    decrypt[ret] = '\0';
+
+    printf("Decrypted message: %s\n", decrypt);
+
+    // Close the device file
     close(fd);
 
     return 0;
